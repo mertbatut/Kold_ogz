ba192acb78506f1d1b3a0fa7e46ceb2f9b2a7146
mertbatut <mertbatut@gmail.com>
2024-06-05 15:10:17 +0300

    Header Mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,16 +22,31 @@ function Header() {
   }, []);
 
   return (
-    <header className={`fixed w-full top-0 left-0 z-10 bg-inherit  ${scrolled ? 'bg-dark-bg text-white shadow-lg' : 'bg-transparent text-white'}`}>
-      <nav className="container mx-auto p-4 flex justify-between items-center  ">
+    <header className={`fixed w-full top-0 left-0 z-10 transition-all duration-300 ${scrolled ? 'bg-dark-bg text-white shadow-lg' : 'bg-transparent text-white'}`}>
+      <nav className="container mx-auto p-4 flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold">Køld</Link>
-        <ul className="flex space-x-4 gap-40">
+        <ul className="hidden md:flex space-x-4">
           <li><Link to="/work" className="hover:underline">Work</Link></li>
           <li><a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">Master Class</a></li>
           <li><a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">Presets</a></li>
           <li><Link to="/contact" className="hover:underline">Contact</Link></li>
         </ul>
+        <div className="md:hidden flex items-center">
+          <button className="mobile-menu-button">
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
       </nav>
+      <div className="mobile-menu hidden md:hidden">
+        <ul className="flex flex-col items-center space-y-4">
+          <li><Link to="/work" className="hover:underline">Work</Link></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">Master Class</a></li>
+          <li><a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">Presets</a></li>
+          <li><Link to="/contact" className="hover:underline">Contact</Link></li>
+        </ul>
+      </div>
     </header>
   );
 }
