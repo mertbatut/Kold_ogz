91886252d5ee7de34228162855690dd2df0f8ef1
mertbatut <mertbatut@gmail.com>
2024-07-08 16:02:56 +0300

    mobile header düzenleme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,15 +22,20 @@ function Header({ scrollToWork, scrollToProducts, scrollToStats, scrollToContact
     };
   }, []);
 
+  const handleMenuClick = (callback) => {
+    callback();
+    setMenuOpen(false);
+  };
+
   return (
     <header className={`fixed w-full top-0 left-0 z-10 transition-all duration-300 ${scrolled ? 'bg-dark-bg text-white shadow-lg' : 'bg-none text-white'}`}>
       <nav className="container mx-auto p-4 flex justify-between items-center">
-        <Link to="/" className="text-2xl font-bold">KØLD</Link>
+        <Link to="/" className="text-2xl font-bold" onClick={() => setMenuOpen(false)}>KØLD</Link>
         <div className="hidden md:flex space-x-4 gap-28">
-          <a onClick={scrollToWork} className="hover:underline cursor-pointer">Work</a>
-          <a onClick={scrollToProducts} className="hover:underline cursor-pointer">Master Class</a>
-          <a onClick={scrollToStats} className="hover:underline cursor-pointer">Presets</a>
-          <a onClick={scrollToContact} className="hover:underline cursor-pointer">Contact</a>
+          <a onClick={() => handleMenuClick(scrollToWork)} className="hover:underline cursor-pointer">Work</a>
+          <a onClick={() => handleMenuClick(scrollToProducts)} className="hover:underline cursor-pointer">Master Class</a>
+          <a onClick={() => handleMenuClick(scrollToStats)} className="hover:underline cursor-pointer">Presets</a>
+          <a onClick={() => handleMenuClick(scrollToContact)} className="hover:underline cursor-pointer">Contact</a>
         </div>
         <div className="md:hidden flex items-center">
           <button onClick={() => setMenuOpen(!menuOpen)} className="mobile-menu-button">
@@ -42,10 +47,10 @@ function Header({ scrollToWork, scrollToProducts, scrollToStats, scrollToContact
       </nav>
       <div className={`md:hidden ${menuOpen ? 'block' : 'hidden'} bg-dark-bg text-white`}>
         <ul className="flex flex-col md:flex-row items-center md:space-x-4 space-y-4 md:space-y-0 p-4">
-          <li><a onClick={scrollToWork} className="hover:underline cursor-pointer">Work</a></li>
-          <li><a onClick={scrollToProducts} className="hover:underline cursor-pointer">Master Class</a></li>
-          <li><a onClick={scrollToStats} className="hover:underline cursor-pointer">Presets</a></li>
-          <li><a onClick={scrollToContact} className="hover:underline cursor-pointer">Contact</a></li>
+          <li><a onClick={() => handleMenuClick(scrollToWork)} className="hover:underline cursor-pointer">Work</a></li>
+          <li><a onClick={() => handleMenuClick(scrollToProducts)} className="hover:underline cursor-pointer">Master Class</a></li>
+          <li><a onClick={() => handleMenuClick(scrollToStats)} className="hover:underline cursor-pointer">Presets</a></li>
+          <li><a onClick={() => handleMenuClick(scrollToContact)} className="hover:underline cursor-pointer">Contact</a></li>
         </ul>
       </div>
     </header>
